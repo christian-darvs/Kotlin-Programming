f9bb4d85d16c7170d14a38809414be9aa9a8b8e2
Christian Darvin <91941006+christian-darvs@users.noreply.github.com>
2022-11-03 06:21:31 +0800

    Create BakeCakeFunction.kt

---
+++ BakeCakeFunction.kt
@@ -0,0 +1,31 @@
+fun main() {
+    bakeCake()
+    prepareIngredients()
+}
+
+fun bakeCake() {
+    println("How To Bake A Cake:")
+    println("1. Grease and Preheat")
+    println("2. Prepare Your Ingredients")
+    println("3. Whisk the Dry Mix")
+    println("4. Cream Your Butter and Sugar")
+    println("5. Add the Eggs")
+    println("6. Time to Combine")
+    println("7. Pour Your Batter in Your Pan")
+    println("8. Bake and Cool Your Cakes")
+    println("9. The Finished Product")
+}
+
+fun prepareIngredients() {
+    println()
+    println("Required Ingredients: ")
+    println("Flour")
+    println("Sugar")
+    println("Salt")
+    println("Baking Powder")
+    println("Eggs")
+    println("Vegetable Oil")
+    println("Butter")
+    println("Milk")
+    println("Vanilla Extract")
+}
