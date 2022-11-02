1b8ff445dcf9c09bba1b3d06e77eba48e90760da
Christian Darvin <91941006+christian-darvs@users.noreply.github.com>
2022-11-03 04:57:57 +0800

    Create FunctionsandReturn.kt

---
+++ FunctionsandReturn.kt
@@ -0,0 +1,23 @@
+fun main() {
+    println(addNumbers(10,2))
+    println(subtractNumbers(12, 3))
+    println(multiplyNumbers(14, 3))
+    println(divideNumbers(14,2))
+    
+}
+
+fun addNumbers(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
+fun subtractNumbers(num1: Int, num2: Int): Int {
+    return num1 - num2
+}
+
+fun multiplyNumbers(num1: Int, num2: Int): Int {
+    return num1 - num2
+}
+
+fun divideNumbers(num1: Int, num2: Int): Int {
+    return num1 / num2
+}
